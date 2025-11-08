7b8d6b399f1799454bc8c8f0e831f72920311806
RaphaelNicaise <raphanicaise@gmail.com>
2025-11-08 18:12:45 -0300

    Renew ssl

--- scripts/renewssl.sh
+++ scripts/renewssl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/fernetloshorneros
-
+docker compose stop nginx || true
 certbot renew --standalone --quiet
 
 cp /etc/letsencrypt/live/fernetloshorneros.com/fullchain.pem /var/www/fernetloshorneros/ssl/
@@ -10,6 +10,7 @@ chown root /var/www/fernetloshorneros/ssl/*
 chmod 644 /var/www/fernetloshorneros/ssl/fullchain.pem
 chmod 600 /var/www/fernetloshorneros/ssl/privkey.pem
 
+docker compose start nginx || docker compose up -d --build nginx
 cd /var/www/fernetloshorneros
 
 echo "$(date): Renovacion ssl completada"
