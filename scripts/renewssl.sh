cb605dedba8fc5fbd7fb1a4ca1a9ee953a1f26ad
RaphaelNicaise <raphanicaise@gmail.com>
2025-12-26 14:50:20 -0300

    Crontab para SSL

--- scripts/renewssl.sh
+++ scripts/renewssl.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cd /var/www/fernetloshorneros
-docker compose stop nginx || true
-certbot renew --standalone --quiet
+set -e
+
+docker compose -f /var/www/fernetloshorneros/docker-compose.yml stop nginx
+
+certbot renew --quiet
 
 cp /etc/letsencrypt/live/fernetloshorneros.com/fullchain.pem /var/www/fernetloshorneros/ssl/
 cp /etc/letsencrypt/live/fernetloshorneros.com/privkey.pem /var/www/fernetloshorneros/ssl/
 
-chown root /var/www/fernetloshorneros/ssl/*
 chmod 644 /var/www/fernetloshorneros/ssl/fullchain.pem
 chmod 600 /var/www/fernetloshorneros/ssl/privkey.pem
 
-docker compose start nginx || docker compose up -d --build nginx
-cd /var/www/fernetloshorneros
+docker compose -f /var/www/fernetloshorneros/docker-compose.yml up -d nginx
 
-echo "$(date): Renovacion ssl completada"
+echo "$(date) SSL renovado OK"
