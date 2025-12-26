9cc01fee82645b2859e9fbc36d32edcd9767b685
RaphaelNicaise <raphanicaise@gmail.com>
2025-12-26 14:54:57 -0300

    .prod

--- scripts/renewssl.sh
+++ scripts/renewssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker compose -f /var/www/fernetloshorneros/docker-compose.yml stop nginx
+docker compose -f /var/www/fernetloshorneros/docker-compose.prod.yml stop nginx
 
 certbot renew --quiet
 
@@ -11,6 +11,6 @@ cp /etc/letsencrypt/live/fernetloshorneros.com/privkey.pem /var/www/fernetloshor
 chmod 644 /var/www/fernetloshorneros/ssl/fullchain.pem
 chmod 600 /var/www/fernetloshorneros/ssl/privkey.pem
 
-docker compose -f /var/www/fernetloshorneros/docker-compose.yml up -d nginx
+docker compose -f /var/www/fernetloshorneros/docker-compose.prod.yml up -d nginx
 
 echo "$(date) SSL renovado OK"
